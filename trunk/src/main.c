a4f001e65c6eea224d59ba28eae80bc7b79c7ea6
zeminlu <zeminlu@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-05 23:05:00 +0000

    Primer subida de cositas

---
+++ trunk/src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (void){
+	int p1, p2;
+	switch (fork()){
+		case 0:
+			execv("./pipe", NULL);
+			break;
+		default:
+			switch (fork()){
+				case 0:
+					execv("./parallel", NULL);
+					break;
+				default:
+					wait(&p1);
+					wait(&p2);
+					break;	
+			}
+			break;
+	}
+	return 0;
+}
---
+++ trunk/src/pipe/pipe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+
+int main(){
+	struct dirent *d;
+	DIR *dp;
+	
+	if ((dp = opendir("./pipeDir")) == NULL){
+		return -1;
+	}
+	while (getFilesAmm(dp) < 3){
+		sleep(1);
+	};
+	printf("Nuevo archivo en pipe\n");
+	return 0;
+}
+
+int getFilesAmm (DIR *dp){
+	struct dirent *d;
+	int ret;
+	while (d = readdir(dp)){
+		 if (d->d_ino != 0){
+			++ret;
+		}
+	}
+	return ret;
+}
