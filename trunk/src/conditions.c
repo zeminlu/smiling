2f3a913763e083bc6e6c6f3fedcc1f4bf8286cba
nicomagni <nicomagni@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 01:41:05 +0000

    conditions listo

--- trunk/src/conditions.c
+++ trunk/src/conditions.c
@@ -1,16 +1,90 @@
 #include "../inc/types.h"
+
+//------------------------sameContinent----------------------------
 int * sameContinent(pais **paises,pais * cabezera){
-	return 0;
+	int * rta;
+	int i, j;
+	
+	rta = malloc(sizeof(int)*31);
+	if(rta == NULL){
+		//	error memoria, insuficiente
+	}
+	for(i = 0, j= 0; i < 31; i++){
+		if(cabezera->continente	== paises[i]->continente){
+			rta[j] = i;
+			j++;
+		}
+	}
+	if(j == 0){
+		return = NULL;
+	}
+	rta = realloc(rta, sizeof(int)*j);
+	return rta;
 }
 
+//------------------------deathGroup----------------------------
 int * deathGroup(pais **paises,pais * cabezera){
-	return 0;
+	int * rta;
+	int i, j;
+	
+	rta = malloc(sizeof(int)*31);
+	if(rta == NULL){
+		//	error memoria, insuficiente
+	}
+	for(i = 0, j= 0; i < 31; i++){
+		if(paises[i]->peso > WEAK_GROUP){
+			rta[j] = i;
+			j++;
+		}
+	}
+	if(j == 0){
+		return = NULL;
+	}
+	rta = realloc(rta, sizeof(int)*j);
+	return rta;
 }
 
+//------------------------champGroup----------------------------
 int * champGroup(pais **paises,pais * cabezera){
-	return 0;
+	int * rta;
+	int i, j;
+	
+	rta = malloc(sizeof(int)*31);
+	if(rta == NULL){
+		//	error memoria, insuficiente
+	}
+	for(i = 0, j= 0; i < 31; i++){
+		if(paises[i]->campeon > 0){
+			rta[j] = i;
+			j++;
+		}
+	}
+	if(j == 0){
+		return = NULL;
+	}
+	rta = realloc(rta, sizeof(int)*j);
+	return rta;
 }
 
+//------------------------weakGroup----------------------------
 int * weakGroup(pais **paises,pais * cabezera){
-	return 0;
+	int * rta;
+	int i, j;
+	
+	rta = malloc(sizeof(int)*31);
+	if(rta == NULL){
+		//	error memoria, insuficiente
+	}
+	for(i = 0, j= 0; i < 31; i++){
+		if(paises[i]->peso <= WEAK_GROUP){
+			rta[j] = i;
+			j++;
+		}
+	}
+	if(j == 0){
+		return = NULL;
+	}
+	rta = realloc(rta, sizeof(int)*j);
+	return rta;
 }
+
