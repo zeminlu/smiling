68e2e7350261fee89f472b0b80257123e94ff925
nicomagni <nicomagni@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 00:48:40 +0000

    return 0

---
+++ trunk/src/conditions.c
@@ -0,0 +1,15 @@
+int * sameContinent(pais **paises,pais * cabezera){
+	return 0;
+};
+
+int * deathGroup(pais **paises,pais * cabezera){
+	return 0;
+};
+
+int * champGroup(pais **paises,pais * cabezera){
+	return 0;
+};
+
+int * weakGroup(pais **paises,pais * cabezera){
+	return 0;
+};
