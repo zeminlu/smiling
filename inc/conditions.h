ba24426a216b2d5584e6c4c348a68188f18a433b
nicomagni <nicomagni@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 00:44:05 +0000

    listooo

---
+++ inc/conditions.h
@@ -0,0 +1,4 @@
+int * sameContinent(pais **paises,pais * cabezera);
+int * deathGroup(pais **paises,pais * cabezera);
+int * champGroup(pais **paises,pais * cabezera);
+int * weakGroup(pais **paises,pais * cabezera);
