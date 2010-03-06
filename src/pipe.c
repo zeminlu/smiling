77c22d42199991c5d6b0e7672613aba6ec8d4a6a
zeminlu <zeminlu@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 19:58:46 +0000

    Compilaaa

--- src/pipe.c
+++ src/pipe.c
@@ -13,8 +13,6 @@
 #include <stdarg.h>
 
 #include "../inc/pipe.h"
-#include "../inc/luGen.h"
-
 
 int main(){
 	
@@ -42,7 +40,7 @@ int main(){
 			if ((initFile = fopen("./pipelDir/inicial.lu", "r")) == NULL){
 				return -1;
 			}
-			initCond = loadInitCond(initFile);
+			initCond = loadInitCondition(initFile);
 		}
 		else if (strcmp(d->d_name, "ecuaciones.lu") == 0){
 			i = 0;
@@ -74,4 +72,98 @@ int getFilesAmm (DIR *dp){
 		}
 	}
 	return ret;
+}
+
+/*
+ *	Carga los datos del archivo inicial.lu y retorna el vector de condiciones iniciales.
+ *	El archivo que llama a esta funcion debe hacer el free de este vector cuando no se utilice mas.
+ */
+
+float * loadInitCondition ( FILE * initFile )
+{
+	int i = 0, cantVar;
+	float *initCond;
+	
+	if( fread(&cantVar, sizeof(int), 1, initFile) == 0 )
+	{
+		printf("Error en la lectura del archivo\n");
+		exit(1);
+	}
+	if( (initCond = (float*)malloc(sizeof(float) * cantVar)) == NULL )
+	{
+		printf("Error en la alocacion de memoria\n");
+		exit(1);
+	}
+	while( feof(initFile) == 0 )
+	{
+		/*printf("i = %d\n",i );*/
+		if( fread(&initCond[i], sizeof(float), 1, initFile) != -1 )
+		{
+			++i;
+		}
+	}
+	fclose(initFile);
+	return initCond;
+}
+
+/*
+ *	Carga los datos del archivo ecuaciones.lu y retorna una matriz con los coeficientes
+ *	de cada ecuacion y los terminos independientes de cada ecuacion. El formato de la 
+ *	matriz es el siguiente:
+ *
+ *			a_0_0	a_0_1 	...		a_0_n	|	b_0
+ *			a_1_0	a_1_1	...		a_1_n	|	b_1
+ *			...
+ *	El programa que lo llama debera hacer el free cuando no se utilice mas.
+ */
+
+float ** loadEquationFile( FILE * ecuFile )
+{
+	int i,j, cantVar;
+	float ** values;
+	
+	i = j = 0;
+	if( feof(ecuFile) == 0 )
+	{
+	 	if( fread(&cantVar, sizeof(int), 1, ecuFile) == 0 )
+		{
+			printf("Error en la lectura del archivo\n");
+			exit(1);
+		}	
+	}
+	
+	++cantVar;
+	if( (values = (float**)malloc(sizeof(float*) * cantVar - 1)) == NULL )
+	{
+		printf("Error en la alocacion de memoria\n");
+		exit(1);
+	}
+	
+	for(i = 0 ; i < cantVar ; ++i)
+	{
+		if( (values[i] = (float*)malloc(sizeof(float) * cantVar)) == NULL )
+		{
+			printf("Error en la alocacion de memoria\n");
+			exit(1);
+		}
+	}
+	
+	i = j = 0;
+	while( feof(ecuFile) == 0 )
+	{
+		/*printf("i = %d  j = %d \n", i,j);*/
+		if( fread(&values[i][j], sizeof(float), 1, ecuFile) != 0 )
+		{
+			if( j == cantVar - 1 )
+			{
+				++i;
+				j = 0;
+			}else
+			{
+				++j;
+			}
+		}
+	}
+	fclose(ecuFile);
+	return values;
 }
