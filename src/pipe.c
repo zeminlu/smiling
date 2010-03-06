53ca646516925eaed79ae7098ff792fb0c8a5c1d
nicospuri <nicospuri@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 19:44:26 +0000

    Se termina la parte de crear los archivos generadores y el programa que lee si se agrego alguna arhivo en el directorio pipeDir

--- src/pipe.c
+++ src/pipe.c
@@ -1,11 +1,28 @@
+/*
+ *		pipe.c
+ *		
+ *			Programa encargado de verificar cuando se agrega un archivo en el directorio
+ *			pipeDir.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <string.h>
+#include <stdarg.h>
+
 #include "../inc/pipe.h"
+#include "../inc/luGen.h"
+
 
 int main(){
+	
 	DIR *dp;
+	struct dirent *d;
+	int i = 0;
+	FILE *ecuFile, *initFile;
+	float **values,*initCond;
 	
 	if ((dp = opendir("./pipeDir")) == NULL){
 		return -1;
@@ -14,8 +31,39 @@ int main(){
 		rewinddir(dp);
 	};
 	printf("Nuevo archivo en pipe\n");
+	rewinddir(dp);
+	while ((d = readdir(dp))){
+		printf("%s\n", d->d_name);
+		if (d->d_ino == 0){
+			continue;
+		}	
+		if (strcmp(d->d_name, "inicial.lu") == 0){
+			i = 0;
+			if ((initFile = fopen("./pipelDir/inicial.lu", "r")) == NULL){
+				return -1;
+			}
+			initCond = loadInitCond(initFile);
+		}
+		else if (strcmp(d->d_name, "ecuaciones.lu") == 0){
+			i = 0;
+			if ((ecuFile = fopen("./pipeDir/ecuaciones.lu", "r")) == NULL){
+				return -1;
+			}
+			values = loadEquationFile(ecuFile);
+		}
+		else if (strcmp(d->d_name, ".") == 0 || strcmp(d->d_name, "..") == 0 || strcmp(d->d_name, ".svn") == 0){
+			continue;
+		}
+		else{
+			printf("Se inserto un arhivo incorrecto\n");
+		}
+	}
 	return 0;
 }
+
+/*
+ *	Verifica la cantidad de archivo que hay dentro del directorio
+ */
 
 int getFilesAmm (DIR *dp){
 	struct dirent *d;
