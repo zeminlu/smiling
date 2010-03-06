ae8ffd85a8dfe7d2705590c8214ebd5ddee1b8f9
nicomagni <nicomagni@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 00:48:40 +0000

    return 0

--- inc/conditions.h
+++ inc/conditions.h
@@ -1,4 +1,7 @@
 int * sameContinent(pais **paises,pais * cabezera);
+
 int * deathGroup(pais **paises,pais * cabezera);
+
 int * champGroup(pais **paises,pais * cabezera);
+
 int * weakGroup(pais **paises,pais * cabezera);
