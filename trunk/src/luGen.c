12f8fddb57a491d109977b4d598d82ac8c97d85a
nicospuri <nicospuri@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-06 19:44:26 +0000

    Se termina la parte de crear los archivos generadores y el programa que lee si se agrego alguna arhivo en el directorio pipeDir

--- trunk/src/luGen.c
+++ trunk/src/luGen.c
@@ -19,6 +19,7 @@ int main (int argc, char const *argv[])
 	int cantVar = 0;
 	float **varValues;
 	float *initCond;
+	FILE *ecuFile, *initFile;
 	
 	printf("Ingrese la cantidad de variables que desea que tenga el sistema\n");
 	
@@ -75,6 +76,21 @@ int main (int argc, char const *argv[])
 	
 	freeAllPtr(varValues, initCond);
 	
+	if ((ecuFile = fopen("./testFiles/ecuaciones.lu", "r")) == NULL){
+		return -1;
+	}
+	if ((initFile = fopen("./testFiles/inicial.lu", "r")) == NULL){
+		return -1;
+	}
+	
+	initCond = loadInitCondition(initFile);
+	varValues = loadEquationFile( ecuFile);
+	
+	printInitCond(initCond);
+	printMatrix(varValues);
+	
+	freeAllPtr(varValues, initCond);
+	
 	return 0;
 }
 
@@ -120,7 +136,7 @@ void createInitCondFile( int cantVar, float *cond )
 
 void createLUFile( float ** values )
 {
-	int i,j;
+	int i,j, cantVar;
 	char * filename = "./testFiles/ecuaciones.lu";
 	FILE * ecu;
 	
@@ -130,9 +146,17 @@ void createLUFile( float ** values )
 		exit(1);
 	}
 	
+	/*printMatrix(values);*/
+	cantVar = sizeof(values)/sizeof(float);
+	if( fwrite(&cantVar, sizeof(int), 1, ecu) == 0 )
+	{
+		printf("Error no se pudo escribir en el archivo inicial.lu\n");
+		fclose(ecu);
+		exit(1);
+	}
 	for( i = 0; i < sizeof(values)/sizeof(float) ; ++i)
 	{
-		for( j = 0; j < sizeof(values[0])/sizeof(float); ++j)
+		for( j = 0; j <= sizeof(values[0])/sizeof(float); ++j)
 		{
 			if( fwrite(&values[i][j], sizeof(float), 1, ecu) == 0 )
 			{
@@ -147,6 +171,10 @@ void createLUFile( float ** values )
 	
 }
 
+/*
+ *	Libera todos los punteros utilizados
+ */
+
 void freeAllPtr( float ** values, float *cond)
 {
 	
@@ -159,3 +187,128 @@ void freeAllPtr( float ** values, float *cond)
 	free(values);
 	free(cond);
 }
+
+/*
+ *	Carga los datos del archivo inicial.lu y retorna el vector de condiciones iniciales.
+ *	El archivo que llama a esta funcion debe hacer el free de este vector cuando no se utilice mas.
+ */
+
+float * loadInitCondition ( FILE * initFile )
+{
+	char * fileName = "./testFiles/inicial.lu";
+	int i = 0, cantVar;
+	float *initCond;
+	
+	if( fread(&cantVar, sizeof(int), 1, initFile) == 0 )
+	{
+		printf("Error en la lectura del archivo\n");
+		exit(1);
+	}
+	if( (initCond = (float*)malloc(sizeof(float) * cantVar)) == NULL )
+	{
+		printf("Error en la alocacion de memoria\n");
+		exit(1);
+	}
+	while( feof(initFile) == 0 )
+	{
+		/*printf("i = %d\n",i );*/
+		if( fread(&initCond[i], sizeof(float), 1, initFile) != -1 )
+		{
+			++i;
+		}
+	}
+	fclose(initFile);
+	return initCond;
+}
+
+/*
+ *	Carga los datos del archivo ecuaciones.lu y retorna una matriz con los coeficientes
+ *	de cada ecuacion y los terminos independientes de cada ecuacion. El formato de la 
+ *	matriz es el siguiente:
+ *
+ *			a_0_0	a_0_1 	...		a_0_n	|	b_0
+ *			a_1_0	a_1_1	...		a_1_n	|	b_1
+ *			...
+ *	El programa que lo llama debera hacer el free cuando no se utilice mas.
+ */
+
+float ** loadEquationFile( FILE * ecuFile )
+{
+	int i,j, cantVar;
+	char * fileName = "./testFiles/ecuaciones.lu";
+	float ** values;
+	
+	i = j = 0;
+	if( feof(ecuFile) == 0 )
+	{
+	 	if( fread(&cantVar, sizeof(int), 1, ecuFile) == 0 )
+		{
+			printf("Error en la lectura del archivo\n");
+			exit(1);
+		}	
+	}
+	
+	++cantVar;
+	if( (values = (float**)malloc(sizeof(float*) * cantVar - 1)) == NULL )
+	{
+		printf("Error en la alocacion de memoria\n");
+		exit(1);
+	}
+	
+	for(i = 0 ; i < cantVar ; ++i)
+	{
+		if( (values[i] = (float*)malloc(sizeof(float) * cantVar)) == NULL )
+		{
+			printf("Error en la alocacion de memoria\n");
+			exit(1);
+		}
+	}
+	
+	i = j = 0;
+	while( feof(ecuFile) == 0 )
+	{
+		/*printf("i = %d  j = %d \n", i,j);*/
+		if( fread(&values[i][j], sizeof(float), 1, ecuFile) != 0 )
+		{
+			if( j == cantVar - 1 )
+			{
+				++i;
+				j = 0;
+			}else
+			{
+				++j;
+			}
+		}
+	}
+	fclose(ecuFile);
+	return values;
+}
+
+void printMatrix( float ** values )
+{
+	
+	int i,j;
+	
+	printf("La matriz es la siguientes:\n");
+	for( i = 0 ; i < sizeof(values)/sizeof(float) ; ++i )
+	{
+		for( j = 0 ; j <= sizeof(values[0])/sizeof(float) ; ++j )
+		{
+			printf("%f  ",values[i][j] );
+		}
+		printf("\n");
+	}
+}
+
+void printInitCond( float *init )
+{
+	int i;
+	
+	printf("Las condiciones iniciales son las siguientes:\n");
+	for(i = 0; i < sizeof(init)/sizeof(float); ++i)
+	{
+		printf("%f ",init[i] );
+	}
+	printf("\n");
+}
+
