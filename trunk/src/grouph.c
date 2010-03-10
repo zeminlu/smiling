0f9ec133112d444394e2108a86169c0c1077b01b
zeminlu <zeminlu@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-10 03:20:23 +0000

    Avanzando con grouph

--- trunk/src/grouph.c
+++ trunk/src/grouph.c
@@ -5,7 +5,8 @@ int main (void){
 	country *data;
 	country **countries;
 	condPack *condArgs;
-	set *group;
+	subFixture *group;
+	set *intersection;
 	int (**conditions)(country **countries, country *head) = NULL;
 	pthread_t *threads;
 	int i = 0, j = 0, index = 0;
@@ -27,27 +28,49 @@ int main (void){
 		conditions[i - 1] = deathGroup;
 	}
 	
-	condArgs = malloc(sizeof(condPack));
-	condArgs->countries = countries;
-	condArgs->head = data;
-	condArgs->index = &index;
-	condArgs->sets = malloc(sizeof(void *) * i);
-	threads = malloc(sizeof(pthread_t) * i);
-	group = malloc(sizeof(set));
-	group->countries = malloc(sizeof(void *));
-	group->countries[0] = data;
-	++(group->countriesAmm);
-	
-	while (group->countriesAmm < 4){
-		for (j = 0 ; j < i ; ++j){
-			pthread_create(&threads[j], NULL, conditions[j], (void *)(condArgs));
+	if (i == 0){
+		/*
+		Funcion que toma un pais random de la tabla de paises y se lo pide a fifa
+		Si fifa lo concede, se realiza lo mismo hasta obtener 3 paises y se devuelve el subFixture
+		*/
+	}
+	else{
+		condArgs = malloc(sizeof(condPack));
+		condArgs->countries = countries;
+		condArgs->head = data;
+		condArgs->index = &index;
+		condArgs->sets = malloc(sizeof(void *) * i);
+		threads = malloc(sizeof(pthread_t) * i);
+		group = malloc(sizeof(subFixture));
+		group->countries = malloc(sizeof(void *));
+		group->countries[0] = data;
+		++(group->countriesAmm);
 
-			/*
-				Se verifica que hayan terminado todos los threads
-				Se realiza la interseccion de los resultados de cada thread
-				Se pide un pais de la interseccion a fifa
-				Si fifa lo concede, se lo agrega a group->countries
-			*/
-		}
+		while (group->countriesAmm < 4){
+			for (j = 0 ; j < i ; ++j){
+				pthread_create(&threads[j], NULL, conditions[j], (void *)(condArgs));
+			}
+			for (j = 0 ; j < i ; ++j){
+				pthread_join(threads[j], NULL);
+			}
+
+			instersection = malloc(sizeof(set));
+			intersection->country = malloc(sizeof(int *) * _MAX_COUNTRIES_);
+			
+			for (j = 0 ; j < ((condArgs->sets)[0])->countriesAmm ; ++j){
+				(intersection->country)[j] = (((condArgs->sets)[0])->country)[j];
+			}
+
+			for (j = 1 ; j < i ; ++j){
+				
+				
+			}
+		/*
+		Se verifica que hayan terminado todos los threads
+		Se realiza la interseccion de los resultados de cada thread
+		Se pide un pais de la interseccion a fifa
+		Si fifa lo concede, se lo agrega a group->countries
+		*/
+	}
 	}
 }
