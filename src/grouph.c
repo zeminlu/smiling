d6af0ee4c55053d74ee0963e724b96885dee7346
nicomagni <nicomagni@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-11 21:39:56 +0000

    Podria decirse que grouph no come memoria...faltan los IPC obviamente

--- src/grouph.c
+++ src/grouph.c
@@ -6,10 +6,9 @@ int main (void){
 	country **countries;
 	condPack *condArgs, **aux;
 	subFixture *group;
-	set *intersection;
 	int (**conditions)(country **countries, country *head) = NULL;
 	pthread_t *threads;
-	int i = 0, j = 0, k = 0, x = 0, y = 0, index = 0, reqCountry;
+	int i = 0, j = 0, k = 0, x = 0, y = 0, index = 0, reqCountry, freePt = 0;
 	
 	if (data->sameContinent){
 		conditions = realloc(conditions, sizeof(void *) * (++i));
@@ -28,14 +27,25 @@ int main (void){
 		conditions[i - 1] = deathGroup;
 	}
 	
-	condArgs = malloc(sizeof(condPack));
+	if ((condArgs = malloc(sizeof(condPack))) == NULL){
+		perror("Error de memoria");
+		return errno;
+	}
+	
 	condArgs->countries = countries;
 	condArgs->head = data;
 	condArgs->index = &index;
-	condArgs->sets = malloc(sizeof(void *) * i);
-	threads = malloc(sizeof(pthread_t) * i);
-	group = malloc(sizeof(subFixture));
-	group->countries = malloc(sizeof(void *));
+	if ((condArgs->sets = malloc(sizeof(void *) * i)) == NULL ||
+		(threads = malloc(sizeof(pthread_t) * i)) == NULL ||
+		(group = malloc(sizeof(subFixture))) == NULL ||
+		(group->countries = malloc(sizeof(void *))) == NULL){
+			free(condArgs->sets);
+			free(threads);
+			free(group);
+			free(conditions);
+			perror("Error de memoria");
+			return errno;
+	}
 	group->countries[0] = data;
 	++(group->countriesAmm);
 	
@@ -56,9 +66,23 @@ int main (void){
 				pthread_join(threads[j], NULL);
 			}
 
-			aux = malloc(sizeof(void *));
-			aux[0] = malloc(sizeof(subFixture));			
-			aux[0]->countries = malloc(sizeof(int *) * ((condArgs->sets)[0])->countriesAmm);
+			if ((aux = malloc(sizeof(void *))) == NULL ||
+				(aux[0] = malloc(sizeof(subFixture))) == NULL ||			
+				(aux[0]->countries = malloc(sizeof(int *) * ((condArgs->sets)[0])->countriesAmm)) == NULL){
+					free(aux[0]);
+					free(aux);
+					free(conditions);
+					free(threads);
+					free(group->countries);
+					free(group);
+					for(j = 0 ; j < i ; ++j){
+						free(condArgs->sets[j]);
+					}
+					free(condArgs->sets);
+					free(condArgs);
+					perror("Error de memoria");
+					return errno;
+				}
 
 			for (j = 0 ; j < ((condArgs->sets)[0])->countriesAmm ; ++j){
 				aux[0]->countries[j] = condArgs->sets[0]->countries[j]; 
@@ -66,9 +90,27 @@ int main (void){
 			aux[0]->countriesAmm = j;
 
 			for (j = 1 ; j < i ; ++j){	
-				aux = realloc(aux, sizeof(void *) * (j + 1));
-				aux[j] = malloc(sizeof(subFixture));
-				aux[j]->countries = malloc(sizeof(int) * aux[0]->countriesAmm);
+				if ((aux = realloc(aux, sizeof(void *) * (j + 1))) == NULL||
+					(aux[j] = malloc(sizeof(subFixture))) == NULL ||
+					(aux[j]->countries = malloc(sizeof(int) * aux[0]->countriesAmm)) == NULL){
+						for(freePt = 0 ; freePT <= j ; ++freePt){
+							free(aux[freePt]->countries);
+							free(aux[freePt]);
+						}
+						free(aux);
+						
+						free(conditions);
+						free(threads);
+						free(group->countries);
+						free(group);
+						for(j = 0 ; j < i ; ++j){
+							free(condArgs->sets[j]);
+						}
+						free(condArgs->sets);
+						free(condArgs);
+						perror("Error de memoria");
+						return errno;
+					}
 				y = 0; 
 				while (x < aux[j - 1]->countriesAmm && k < ((condArgs->sets)[j])->countriesAmm) {  
 					if (((aux[j - 1])->countries)[x] == (((condArgs->sets)[j])->countries)[k]) {  
@@ -92,8 +134,21 @@ int main (void){
 		Se pide a fifa reqCountry
 		Si lo concede, se agrega a group y se incrementa la cantidad de paises en el mismo
 		*/
+		for (j = 0 ; j < i ; ++j){
+			free(condArgs->sets[j]->countries);
+			free(condArgs->sets[j]);
+		}
+		for (x = 0 ; x < i ; ++ x){
+			free(aux[x]->countries);
+			free(aux[x]);
+		}
 	}
 	/*
 	Se envia a fifa el subfixture terminado, o error de no haberse logrado
 	*/
+	free(condArgs->sets);
+	free(condArgs);
+	free(conditions);
+	free(threads);
+	return 0;
 }
