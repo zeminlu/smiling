723e274fd94e54c1bcd5ade9a14752b01d674658
zeminlu <zeminlu@84bd5d20-28a0-11df-bb46-9574ba5d0bfa>
2010-03-20 20:33:27 +0000

    Agrego pipeIPC.h

---
+++ trunk/inc/pipeIPC.h
@@ -0,0 +1,30 @@
+#ifndef IPCAPI_H_
+#define IPCAPI_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include "../inc/definitions.h"
+#include "../inc/types.h"
+#include "../inc/linearHashADT.h"
+#include "../inc/ipcAPI.h"
+
+int compareIPCIDs(void *elem1, void *elem2);
+
+void * copyIPCID(void *elem);
+
+void freeIPCID(void *elem);
+
+void itoa(int n, char s[]);
+
+void reverse(char s[]);
+
+#endif
